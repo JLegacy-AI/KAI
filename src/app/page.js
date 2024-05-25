52365277cb6cbbbd5baf74d4d8a85018b483dc13
Arslan <arslanjs.dev@gmail.com>
2024-05-25 13:52:37 +0500

    Remove old landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
-import LandingPage from "@/components/landing/LandingPage";
+import { Heading, Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 export default function Home() {
 
   return (
-    <main className="h-screen w-screen overlfow-y-auto bg-white text-black">
-      <LandingPage />
+    <main className="h-screen w-screen overlfow-y-auto bg-white text-black flex flex-col items-center justify-center">
+      <Heading size="9">KAI</Heading>
+      <Button>
+        <Link href="/login">Get Started</Link>
+      </Button>
     </main>
   );
 }
